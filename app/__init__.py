0f32936c248b866a247a1640d191022cc47ce3e2
isaak <yandil03@evergreen.edu>
2015-06-15 15:34:44 -0700

    adding db configs for mysql

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask.ext.mysqldb import MySQL
 
 app = Flask(__name__)
-# import placed here to avoid circular references
+mysql = MySQL()
+app.config['MYSQL_USER'] = 'report'
+app.config['MYSQL_PASSWORD'] = '***REMOVED***'
+app.config['MYSQL_DB'] = 'ORG1'
+app.config['MYSQL_PORT'] = 3306
+mysql.init_app(app)
+
 from app import views
