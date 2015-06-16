75ab7f1f4cd09cd5133d051fba665ca6409ee6c9
isaak <yandil03@evergreen.edu>
2015-06-16 11:31:49 -0700

    parameterize sql query

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from MySQLdb.cursors import DictCursor
+from flask import render_template
 from app import app
 from app import mysql
 
@@ -5,7 +7,19 @@ from app import mysql
 @app.route('/')
 @app.route('/index')
 def index():
-    cur = mysql.connection.cursor()
-    cur.execute('select * from HD_TICKET')
-    result = cur.fetchall()
-    return str(result)
+    """ home page view, basically all of the stuff will go here.
+    """
+    # create cursor from database connection, which returns
+    # entres as dictionary objects.
+    cur = mysql.connection.cursor(cursorclass=DictCursor)
+    # query for all the tickets from last week
+    cur.execute('select * from HD_TICKET where MODIFIED < NOW() - INTERVAL 1 WEEK')
+    results = [e for e in cur.fetchall() if ]
+    # render the template and pass in most recent ticket changes
+    return render_template('timeline.html',
+                           title='kaceline')
+
+
+@app.route('/changes')
+def changes():
+    return NotImplementedError
