95c85a1918b8565363d3c6110a8b75efe8e89301
Noah Weiner <noahbweiner@gmail.com>
2016-02-10 21:19:55 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from flask.ext.mysqldb import MySQL
 
 app = Flask(__name__)
 mysql = MySQL()
-app.config['MYSQL_USER'] = 'report'
+app.config['MYSQL_USER'] = '*****'
 app.config['MYSQL_HOST'] = 'kaceapp'
-app.config['MYSQL_PASSWORD'] = '***REMOVED***'
+app.config['MYSQL_PASSWORD'] = '*****'
 app.config['MYSQL_DB'] = 'ORG1'
 app.config['MYSQL_PORT'] = 3306
 mysql.init_app(app)
